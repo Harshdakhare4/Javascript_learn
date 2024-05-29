d2dfd760a3fad193d138e2096940b04fb98406d0
Harsh Dakhare <121993868+Harshdakhare4@users.noreply.github.com>
2024-05-29 06:22:40 +0000

    datatypes

---
+++ JS/1/datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser we can use it in the browser
+
+
+console.log(3 + 3) 
+
+console.log("Harsh")
+let name = "Harsh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => not assigned any value to the variable
+// symbol => unique
+
+
+//object
+console.log(typeof(null));//object
+console.log(typeof(undefined));//undefined
