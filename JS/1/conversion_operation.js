3b68dd1ac0694c721ce21e040d5ba7b5d05eb8a9
Harsh Dakhare <121993868+Harshdakhare4@users.noreply.github.com>
2024-06-04 10:10:13 +0000

    conversion_datatypes

---
+++ JS/1/conversion_operation.js
@@ -0,0 +1,19 @@
+let score="33abc";
+console.log(typeof score);
+let valueinNumber=Number(score);
+//console.log(typeof valueinNumber);
+//console.log(valueinNumber);
+// "33"=>33
+//"33abc"=>NaN
+//true=>1  ;false=>0
+
+let isLoggedIn =10
+//let isLoggedIn ="harsh"
+//let booleanIsLoggedIn=Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn)
+//console.log(typeof booleanIsLoggedIn)
+let booleanintoString=String(isLoggedIn)
+
+console.log(typeof booleanintoString)
+console.log( booleanintoString)
+
