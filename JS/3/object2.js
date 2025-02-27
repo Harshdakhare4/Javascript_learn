1823f7fc3ce1484a36490f4245f80ec16df25a09
Harsh Dakhare <121993868+Harshdakhare4@users.noreply.github.com>
2025-02-27 20:34:13 +0000

    Arrow.js

--- JS/3/object2.js
+++ JS/3/object2.js
@@ -23,7 +23,7 @@ const obj2 ={3:"a",4:"b"}
 // const obj3={obj1 , obj2}
 // console.log(obj3);
 
-const obj4=Object.assign(obj1,obj2);
+const obj4=Object.assign(obj2);
 //Syntax: Object.assign(target,source) here target is obj1
 const obj5=Object.assign({},obj1,obj2);//obj1 and obj2 are source
 
@@ -31,7 +31,7 @@ const obj5=Object.assign({},obj1,obj2);//obj1 and obj2 are source
 //  console.log(obj1);
 
 const obj6={...obj1,...obj2}
-// console.log(obj6);
+// console.log(obj6);obj1,o
 
 
 const users = [
